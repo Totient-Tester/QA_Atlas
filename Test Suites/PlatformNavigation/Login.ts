18a25a781e74a0aed2efd974f7a02bcca2664e2e
Totient-Tester <katalontotient@gmail.com>
2020-03-19 15:12:56 +0530

    added login test suites

---
+++ Test Suites/PlatformNavigation/Login.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>b4de71cd-d99f-43f8-9612-490e70c634dd</testSuiteGuid>
+   <testCaseLink>
+      <guid>4b2b358e-91e0-402f-a4bd-f7365de716de</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/PlatformNavigation/Login</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
