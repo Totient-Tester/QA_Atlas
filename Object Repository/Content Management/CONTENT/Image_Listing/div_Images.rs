9fd28658aeb1e5a6bc31eb3b8dc6d9f660e03637
Totient-Tester <katalontotient@gmail.com>
2020-03-26 16:33:10 +0530

    modified xpath of the images nav

--- Object Repository/Content Management/CONTENT/Image_Listing/div_Images.rs
+++ Object Repository/Content Management/CONTENT/Image_Listing/div_Images.rs
@@ -7,7 +7,7 @@
    <selectorCollection>
       <entry>
          <key>XPATH</key>
-         <value>(//a/*[contains(text(),'Images')])[1]</value>
+         <value>(//*[contains(text(),'Images')])[1]</value>
       </entry>
    </selectorCollection>
    <selectorMethod>XPATH</selectorMethod>
